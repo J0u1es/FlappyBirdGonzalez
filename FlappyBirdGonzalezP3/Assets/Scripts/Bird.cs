41bb0a6435af6fefb098ee3604c85666dbaa7f94
Julia Gonzalez <145380988+J0u1es@users.noreply.github.com>
2023-12-14 11:40:53 -0800

    Completed flappy bird completed almost
    I finished programming the columns and added some audio to the game. I just need to add the score sound and that's it.

--- FlappyBirdGonzalezP3/Assets/Scripts/Bird.cs
+++ FlappyBirdGonzalezP3/Assets/Scripts/Bird.cs
@@ -9,11 +9,16 @@ public class Bird : MonoBehaviour
     public float upForce = 200f;
     private Animator anim;
 
+    AudioSource audioSource;
+    public AudioClip flapSound;
+    public AudioClip deathSound;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D> ();
         anim = GetComponent<Animator> ();
+        audioSource = GetComponent<AudioSource> ();
     }
 
     // Update is called once per frame
@@ -26,6 +31,7 @@ public class Bird : MonoBehaviour
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upForce));
                 anim.SetTrigger("Flap");
+                PlaySound(flapSound);
             }
         }
     }
@@ -35,5 +41,13 @@ public class Bird : MonoBehaviour
         isDead = true;
         anim.SetTrigger("Die");
         GameController.instance.BirdDied();
+        PlaySound(deathSound);
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 }
+
+
